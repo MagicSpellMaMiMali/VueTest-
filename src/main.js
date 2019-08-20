84647bf5d749b8b62ce24262d432fb0017c8d17d
郭嘉 <944925645@qq.com>
2019-08-20 22:01:31 +0800

    尝试性修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui' // 引入element组件
+import 'element-ui/lib/theme-chalk/index.css' // elements引入样式
+// import './styles/index.less' // 引入重置的基本样式
 
+Vue.use(ElementUi) // 注册elements组件
 Vue.config.productionTip = false
 
 new Vue({
