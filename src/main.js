5baae7f35703bf6a833cc1e13e42bedfb9c064c1
郭嘉 <944925645@qq.com>
2019-08-21 01:24:39 +0800

    less文件报错位置项目

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUi from 'element-ui' // 引入element组件
-import 'element-ui/lib/theme-chalk/index.css' // elements引入样式
+import 'element-ui/lib/theme-chalk/index.css' // element引入样式
 // import './styles/index.less' // 引入重置的基本样式
 
 Vue.use(ElementUi) // 注册elements组件
